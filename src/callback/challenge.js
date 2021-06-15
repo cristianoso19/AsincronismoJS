93a66c84e8e9b2019e0bd89e4ededa45fa447333
Cristian Fedora <cristianoso19@gmail.com>
2021-06-14 21:48:52 -0500

    Callbacks

---
+++ src/callback/challenge.js
@@ -0,0 +1,48 @@
+// objeto para hacer peticiones a url o api
+// puede trabajar con ftp o archivos<
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let API = "https://rickandmortyapi.com/api/character/";
+
+function fetchData(url_api, callback) {
+  // Creado por Microsoft
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("GET", url_api, true);
+  xhttp.onreadystatechange = function(event) {
+    // si el estado se encuentra es satisfactorio
+    // tiene 5 estados 0 no se ha ejecutado 4 se ha
+    // completado
+    if (xhttp.readyState === 4) {
+      // ejecutar y comparar status
+      // 200 todo esta bien
+      // 500 servidor fallo
+      // 400 no lo encontro
+      if (xhttp.status === 200) {
+        // Callback usa standard que permite
+        // decir el primero es el error
+        // y el segundo es el resultado
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error("Error" + url_api);
+        return callback(error, null);
+      }
+    }
+  };
+  xhttp.send();
+}
+
+// Multiples Peticiones a un API con Callbacks
+fetchData(API, function(error1, data1) {
+  if (error1)
+    return console.error(error1);
+  fetchData(API + data1.results[0].id, function(error2, data2) {
+    if (error2)
+      return console.error(error2);
+    fetchData(data2.origin.url, function(error3, data3) {
+      if (error3)
+        return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  });
+});
